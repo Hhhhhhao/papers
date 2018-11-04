fd51218b9cf2df4ef6131b77081498bdcedeb803
Hao <s1786991@sms.ed.ac.uk>
2018-11-04 15:27:06 +0000

    update GAN  code

--- GAN/gan.py
+++ GAN/gan.py
@@ -1,21 +1,60 @@
 from keras.datasets import mnist
-from keras.layers import Input, Dense, Reshape, Flatten, Dropout, BatchNormalization, Activation
+from keras.layers import Input, Dense, Reshape, Flatten, BatchNormalization, Activation
 from keras.layers.advanced_activations import LeakyReLU
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 from keras.optimizers import Adam
+from keras.callbacks import TensorBoard
 
 import matplotlib.pyplot as plt
+import tensorflow as tf
 import seaborn as sns
 import numpy as np
+import os
+
+
+def write_log(callback, names, logs, batch_no):
+    """
+
+    :param callback: keras tensorboard callback
+    :param names: loss names
+    :param logs: loss values
+    :param batch_no: num of iterations
+    :return: None
+    """
+    for name, value in zip(names, logs):
+        summary = tf.Summary()
+        summary_value = summary.value.add()
+        summary_value.simple_value = value
+        summary_value.tag = name
+        callback.writer.add_summary(summary, batch_no)
+        callback.writer.flush()
+
+
+def create_dir(dir):
+    """
+    :param dir:directory to create if these directories are not found
+    :return exit_code: 0:success -1:failed
+    """
+    try:
+        if not os.path.exists(dir):
+            os.makedirs(dir)
+        return dir
+    except Exception as err:
+        print('Creating directory error: {0}'.format(err))
+        exit(-1)
 
 
 class GAN:
     def __init__(self):
 
+        self.output_dir = create_dir('./output/')
+        self.tensorboard_dir = create_dir('./output/log/')
+        self.weights_dir = create_dir('./output/weights/')
+        self.samples_dir = create_dir('./output/samples/')
+
         (self.x_train, _), (self.x_test, _) = mnist.load_data()
         self.x_train = np.expand_dims(self.x_train, axis=-1)
-        print(self.x_train.shape)
         self.x_test = np.expand_dims(self.x_test, axis=-1)
 
         self.img_rows = 28
@@ -24,8 +63,8 @@ class GAN:
         self.img_shape = (self.img_rows, self.img_cols, self.channels)
         self.latent_dim = 100
 
-        g_optimizer = Adam(0.0002, beta_1=0.5)
-        d_optimizer = Adam(0.0002, beta_1=0.5)
+        g_optimizer = Adam(0.001)
+        d_optimizer = Adam(0.004)
 
         # build and compile the discriminator
         self.discriminator = self.build_discriminator()
@@ -35,12 +74,10 @@ class GAN:
         self.generator = self.build_generator()
 
         # The generator takes noise as input and generate imgs
-        z = Input(shape=(self.latent_dim,))
-        img = self.generator(z)
-
         # for the combined model we only train the generator
         self.discriminator.trainable = False
-
+        z = Input(shape=(self.latent_dim,))
+        img = self.generator(z)
         # the discriminator takes generated images as input and determines validity
         validity = self.discriminator(img)
 
@@ -74,13 +111,13 @@ class GAN:
 
         x_input = Input(shape=self.img_shape, name='d_input0')
         x = Flatten(name='d_flatten0')(x_input)
-        x = Dense(1024, name='d_dense1')(x)
+        x = Dense(512, name='d_dense1')(x)
         x = BatchNormalization(name='d_bn2')(x)
         x = LeakyReLU(alpha=0.2, name='d_ac1')(x)
-        x = Dense(512, name='d_dense2')(x)
+        x = Dense(256, name='d_dense2')(x)
         x = BatchNormalization(name='d_bn2')(x)
         x = LeakyReLU(alpha=0.2, name='d_ac2')(x)
-        x = Dense(256, name='d_dense3')(x)
+        x = Dense(128, name='d_dense3')(x)
         x = BatchNormalization(name='d_bn3')(x)
         x = LeakyReLU(alpha=0.2, name='d_ac3')(x)
         x_output = Dense(1, activation='sigmoid', name="d_dense4")(x)
@@ -90,108 +127,97 @@ class GAN:
 
         return model
 
-    def train(self, epochs, batch_size=32, label_smooth=False, sample_intervals=100):
-        # initialize losses dict
-        losses = {"g_loss": [], "d_loss": [], "val_loss": [], "d_acc_real": [], "d_acc_fake": [], "val_real_acc": [],
-                  "val_fake_acc": []}
+    def train(self, epochs, batch_size=32):
 
-        train_datagen = ImageDataGenerator(
-            rescale=1/255.)
-        val_datagen = ImageDataGenerator(rescale=1/255.)
+        # initialize losses names and callback
+        tensorboard = TensorBoard(self.tensorboard_dir, batch_size=16, write_graph=True, write_images=True)
+        train_generator_names = ['g_bce']
+        train_discriminator_names = ['d_bce', 'd_acc']
+        valid_names = ['valid_g_bce', 'valid_d_bce', 'valid_d_acc']
 
+        # initialize data generators
+        train_datagen = ImageDataGenerator(rescale=1/255.)
+        valid_datagen = ImageDataGenerator(rescale=1/255.)
         train_datagen.fit(self.x_train)
-        val_datagen.fit(self.x_test)
-
+        valid_datagen.fit(self.x_test)
         train_generator = train_datagen.flow(self.x_train, batch_size=batch_size)
-        val_generator = val_datagen.flow(self.x_test, batch_size=batch_size)
+        valid_generator = valid_datagen.flow(self.x_test, batch_size=batch_size)
 
         # start training
+        total_iteration = 0
         for epoch in range(epochs):
-            print('-' * 15, 'Epoch %d of %f' % (epoch+1, epochs), '-' * 15)
-            for t in range(len(train_generator)):
-
-                # generate batch data
-                imgs = train_generator[t]
-
+            print('-' * 15, 'Epoch {0} of {1}'.format(epoch+1, epochs), '-' * 15)
+            epoch_iteration = 0
+            for x_train in train_generator:
+                total_iteration += 1
+                epoch_iteration += 1
                 # ----------------------
                 #  Train Discriminator
                 # ----------------------
                 self.discriminator.trainable = True
-
-                # define labels
-                if label_smooth:
-                    valid = np.ones((imgs.shape[0], 1)) * 0.9
-                else:
-                    valid = np.ones((imgs.shape[0], 1))
-
-                fake = np.zeros((imgs.shape[0], 1))
-
+                valid = np.ones((x_train.shape[0], 1))
+                fake = np.zeros((x_train.shape[0], 1))
+                labels = np.concatenate((valid,fake), axis=0)
                 # generate noise
-                noise = np.random.normal(0, 1, (imgs.shape[0], self.latent_dim))
-
+                noise = np.random.normal(0, 1, (x_train.shape[0], self.latent_dim))
                 # generate a batch of new images from noise
                 gen_imgs = self.generator.predict(noise)
-
+                imgs = np.concatenate((x_train, gen_imgs), axis=0)
                 # train the discriminator
-                d_loss_real = self.discriminator.train_on_batch(imgs, valid)
-                d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
-                d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-                losses["d_loss"].append(d_loss[0])
-                losses["d_acc_real"].append(100 * d_loss_real[1])
-                losses["d_acc_fake"].append(100 * d_loss_fake[1])
+                d_metrics = self.discriminator.train_on_batch(imgs, labels)
+                d_bce = d_metrics[0]
+                d_acc = d_metrics[1] * 100
+                d_losses = [d_bce, d_acc]
+                write_log(tensorboard, train_discriminator_names, d_losses, total_iteration)
 
                 # ----------------------
                 #  Train Generator
                 # ----------------------
                 self.discriminator.trainable = False
                 # generate noise and valid labels
-                noise = np.random.normal(0, 1, (imgs.shape[0], self.latent_dim))
-                valid = np.ones((imgs.shape[0], 1))
+                noise = np.random.normal(0, 1, (x_train.shape[0], self.latent_dim))
+                valid = np.ones((x_train.shape[0], 1))
                 # train the generator
                 # flip the target to resolve the gradient vanish problem
-                g_loss = self.combined.train_on_batch(noise, valid)
-                losses["g_loss"].append(g_loss)
+                g_bce = self.combined.train_on_batch(noise, valid)
+                write_log(tensorboard, train_generator_names, [g_bce], total_iteration)
 
                 # Plot the progress at certain sample intervals
-                if t%sample_intervals == 0 or t == len(train_generator)-1:
-                    print("iteration:%d [D loss: %a, real acc.: %.2f%%, fake acc.: %.2g%%] [G loss: %g] "
-                        % (t, d_loss[0], 100 * d_loss_real[1], 100 * d_loss_fake[1], g_loss))
+                if epoch_iteration%100 == 0 or epoch_iteration == len(train_generator)-1:
+                    print("t:{0}/{1} [D bce_loss:{2:.4f}, acc.:{3:.2f}%] [G bce_loss: {4:.4f}]".format
+                          (epoch_iteration, len(train_generator)-1,
+                           d_bce, d_acc, g_bce))
 
             # validation at the end of the epoch
-            val_losses = {"g_loss": [], "d_loss": [], "d_acc_real": [], "d_acc_fake": []}
-            for t in range(len(val_generator)):
-                # generate validation batch
-                imgs = val_generator[t]
-
+            val_losses = {"g_bce": [], "d_bce": [], "d_acc": []}
+            for x_valid in valid_generator:
                 # validate discriminator and generator
-                noise = np.random.normal(0, 1, (imgs.shape[0], self.latent_dim))
+                noise = np.random.normal(0, 1, (x_valid.shape[0], self.latent_dim))
                 gen_imgs = self.generator.predict(noise)
-                valid = np.ones((imgs.shape[0], 1))
-                fake = np.zeros((imgs.shape[0], 1))
-                d_loss_real = self.discriminator.test_on_batch(imgs, valid)
-                d_loss_fake = self.discriminator.test_on_batch(gen_imgs, fake)
-                g_loss = self.combined.test_on_batch(noise, valid)
-                d_loss = 0.5 * np.add(d_loss_fake[0], d_loss_real[0])
-                val_losses["d_loss"].append(d_loss)
-                val_losses["d_acc_real"].append(d_loss_real[1] * 100)
-                val_losses["d_acc_fake"].append(d_loss_fake[1] * 100)
-                val_losses["g_loss"].append(g_loss)
-            losses["val_loss"] += val_losses["g_loss"]
-            losses["val_real_acc"] += val_losses["d_acc_real"]
-            losses["val_fake_acc"] += val_losses["d_acc_fake"]
-            losses["val_loss"] += val_losses["g_loss"]
-            # Plot the progress
-            print("validation [D loss: %a, real acc.: %.2f%%, fake acc.:%.2g%%] [G loss: %g] "
-                  % (np.mean(val_losses["d_loss"]), np.mean(val_losses["d_acc_real"]), np.mean(val_losses["d_acc_fake"]), np.mean(val_losses["g_loss"])))
+                imgs = np.concatenate((x_valid, gen_imgs), axis=0)
+                valid = np.ones((x_valid.shape[0], 1))
+                fake = np.zeros((x_valid.shape[0], 1))
+                labels = np.concatenate((valid,fake), axis=0)
+                d_metrics = self.discriminator.test_on_batch(imgs, labels)
+                d_bce = d_metrics[0]
+                d_acc = d_metrics[1] * 100
+                g_bce = self.combined.test_on_batch(noise, valid)
+                val_losses["d_bce"].append(d_bce)
+                val_losses["d_acc"].append(d_acc)
+                val_losses["g_bce"].append(g_bce)
+            valid_losses = [np.mean(val_losses["g_bce"]), np.mean(val_losses["d_bce"]), np.mean(val_losses["d_acc"])]
+            write_log(tensorboard, valid_names, valid_losses, epoch+1)
+            print("validation epoch:{0}/{1} [D bce_loss:{2:.4f}, acc.:{3:.2f}%] [G bce_loss: {4:.4f}]".format
+                  (epoch+1, epochs,
+                   valid_losses[0], valid_losses[1], valid_losses[2]))
             # save generated samples at epoch end
             self.sample_images(epoch+1)
 
         self.generator.save_weights(filepath="./weights/generator.hdf5")
         self.discriminator.save_weights(filepath="./weights/discriminator.hdf5")
 
-        return losses
-
     def sample_images(self, epoch):
+
         r, c = 5, 5
         np.random.seed(1)
         noise = np.random.normal(0, 1, (r*c, self.latent_dim))
@@ -205,34 +231,12 @@ class GAN:
                 axs[i,j].imshow(gen_imgs[count, :, :, 0], cmap='gray')
                 axs[i,j].axis('off')
                 count += 1
-            fig.savefig("samples/%d.png" % epoch)
+            fig.savefig("./output/samples/{0}.png".format(epoch))
             plt.close()
 
 
 if __name__ == '__main__':
     gan = GAN()
-    losses = gan.train(epochs=10, batch_size=128, label_smooth=False, sample_intervals=100)
-
-    color = ['b', 'g', 'tab:orange', 'r', 'r', 'r', 'r']
-    sns.set(color_codes=True)
-    sns.set_style("white")
-    sns.set_context("notebook", font_scale=1, rc={"lines.linewidth": 1})
-
-    for i, key in enumerate(losses.keys()):
-        if i <= 2:
-            plt.plot(losses[key], color[i])
-            plt.xlabel('iterations')
-            plt.ylabel('loss')
-            plt.savefig("./losses/" + key[1] + ".png")
-            plt.show()
-            plt.close()
-        else:
-            plt.plot(losses[key], color[i])
-            plt.ylim((0,100))
-            plt.xlabel('iterations')
-            plt.ylabel('accuracy')
-            plt.savefig("./losses/" + key[1] + ".png")
-            plt.show()
-            plt.close()
+    gan.train(epochs=100, batch_size=128)
 
 
